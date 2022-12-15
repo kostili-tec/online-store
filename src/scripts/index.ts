099fbb0951761ee085030b10c5435ea3c0233c0b
kostili-tec <weh4teyou@gmail.com>
2022-12-15 21:49:22 +0300

    feat: add footer & styles

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,8 +1,9 @@
 import '../styles/main.scss';
 import { createHeader } from './components/header';
+import { createFooter } from './components/footer';
 
 const start = () => {
-  document.body.append(createHeader());
+  document.body.append(createHeader(), createFooter());
 };
 
 start();
