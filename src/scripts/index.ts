93400696712467ef60e439229c88b2e1065ce885
ferka123 <ferka911@gmail.com>
2022-12-18 14:56:55 +0200

    fix: remove topbar import from index

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -2,8 +2,6 @@ import '../styles/main.scss';
 import { createHeader } from './components/header';
 import { createFooter } from './components/footer';
 import { root } from './router';
-import { ProductsTopbar } from './components/ProductsTopbar';
-root.append(ProductsTopbar({}));
 
 const start = () => {
   document.body.append(createHeader(), root, createFooter());
