0ec015fa12a1ab38d95c29ba0266da5dfe2cd121
kotili-tec <weh4teyou@gmail.com>
2022-12-19 16:18:18 +0300

    feat: details page. styles and render details.

--- src/scripts/pages/ProductDetails.ts
+++ src/scripts/pages/ProductDetails.ts
@@ -39,22 +39,29 @@ function createProductDetails(product: IProduct) {
   }
 
   const detailsInfoContainer = document.createElement('div');
-  detailsInfoContainer.classList.add('details__info-container');
+  detailsInfoContainer.classList.add('details__info-container', 'info-container');
 
   const headerProduct = document.createElement('h2');
+  headerProduct.classList.add('info-container__title-h2');
   headerProduct.textContent = product.title;
 
-  const ratingP = document.createElement('p');
-  ratingP.textContent = `${product.rating}/5`;
+  const rating = document.createElement('div');
+  rating.classList.add('info-container__rating', 'card-info__rating');
+  const ratingPercent = product.rating ? (product.rating * 100) / 5 : 0;
+  rating.style.backgroundImage = `linear-gradient(to right, #151515 0%, #151515 ${ratingPercent}%, #ccc ${ratingPercent}%)`;
+  rating.title = product.rating.toString();
 
   const descriptionP = document.createElement('p');
+  descriptionP.classList.add('info-container__description');
   descriptionP.textContent = product.description;
 
   const brand = createProductSpec('Brand', product.brand);
   const category = createProductSpec('Category', product.category);
   const stock = createProductSpec('Stock', product.stock);
 
-  detailsInfoContainer.append(headerProduct, ratingP, descriptionP, brand, category, stock);
+  const productBuy = createProductBuy(product.price, product.discountPercentage);
+
+  detailsInfoContainer.append(headerProduct, rating, descriptionP, brand, category, stock, productBuy);
   productContainer.append(imagesContainer, detailsInfoContainer);
   return productContainer;
 }
@@ -73,12 +80,38 @@ function createProductSpec(specTitle: string, specValue: string | number): HTMLD
   return container;
 }
 
-function createProductBuy(price: number, discount: number | null) {
+function createProductBuy(price: number, discount: number | null): HTMLDivElement {
   const buyContainer = document.createElement('div');
   buyContainer.classList.add('info-container__buy-container');
 
   const priceContainer = document.createElement('div');
   priceContainer.classList.add('buy-container__prices-container');
+
+  const priceWithDiscount = document.createElement('p');
+  priceWithDiscount.classList.add('card-pricing__discounted', 'prices-container__discount');
+  const priceOriginal = document.createElement('p');
+  priceOriginal.classList.add('card-pricing__original', 'prices-container__original');
+  priceOriginal.textContent = `${price} USD`;
+  if (discount) {
+    priceWithDiscount.textContent = `${(price - price * (discount / 100)).toFixed(2)} USD`;
+    priceContainer.append(priceWithDiscount, priceOriginal);
+  } else {
+    priceContainer.append(priceOriginal);
+  }
+  const buyButtonsContainer = document.createElement('div');
+  buyButtonsContainer.classList.add('buy-container__buttons-container');
+
+  const buyNowButton = document.createElement('button');
+  buyNowButton.classList.add('buttons-container__button', 'primary-button');
+  buyNowButton.textContent = 'Buy Now';
+  const addButton = document.createElement('button');
+  addButton.classList.add('buttons-container__button', 'secondary-button');
+  addButton.textContent = 'Add to cart';
+
+  buyButtonsContainer.append(buyNowButton, addButton);
+
+  buyContainer.append(priceContainer, buyButtonsContainer);
+  return buyContainer;
 }
 
 /* function createError() {} */
