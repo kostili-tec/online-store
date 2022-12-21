47aee9f378f6e7efdf7fcfbf1a07b93c0cc713a4
kotili-tec <weh4teyou@gmail.com>
2022-12-21 15:22:07 +0300

    feat: finish styles and events for product cards in cart

--- src/scripts/pages/Cart.ts
+++ src/scripts/pages/Cart.ts
@@ -5,6 +5,7 @@ import { getProducts } from '../testApi';
 import { IProduct } from '../testApi';
 import { navigate } from '../router';
 import { createProductSpec } from './ProductDetails';
+import sprite from '../../assets/svg/sprite.svg';
 
 export async function Cart(container: HTMLElement) {
   const cartItems = store.cart.getItemsAll();
@@ -34,7 +35,7 @@ export async function Cart(container: HTMLElement) {
 
 function cartProductCard(product: IProduct): HTMLElement {
   const container = document.createElement('div');
-  container.className = 'product-card';
+  container.classList.add('product-card', 'cart-product__card');
 
   const discountPercentage = Math.round(product.discountPercentage);
   if (discountPercentage) {
@@ -45,6 +46,7 @@ function cartProductCard(product: IProduct): HTMLElement {
   }
 
   const leftContainer = document.createElement('div');
+  leftContainer.classList.add('prodcur-card__left-container');
 
   const productImage = document.createElement('img');
   productImage.className = 'product-card__image';
@@ -52,15 +54,27 @@ function cartProductCard(product: IProduct): HTMLElement {
   productImage.alt = product.title;
   productImage.onclick = (e) => navigate('/product?=' + product.id, e);
 
-  const removeButton = document.createElement('a');
-  removeButton.textContent = 'x Remove';
+  const crossSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  crossSvg.classList.add('icon-svg', 'cross-svg');
+  const useSvg = document.createElementNS('http://www.w3.org/2000/svg', 'use');
+  useSvg.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', `${sprite}#cross`);
+  crossSvg.appendChild(useSvg);
+  const removeButton = document.createElement('button');
+  removeButton.classList.add('prodcut-card__remove-button');
+  removeButton.appendChild(crossSvg);
 
-  leftContainer.append(productImage, removeButton);
+  leftContainer.append(productImage);
+
+  removeButton.addEventListener('click', () => {
+    store.cart.delete(product.id);
+    container.remove();
+  });
 
   const productInfo = document.createElement('div');
   productInfo.className = 'product-card__info';
   const title = document.createElement('h4');
   title.className = 'card-info__title';
+  title.classList.add('card-info__title', 'product-card__title');
   title.textContent = product.title;
   title.onclick = (e) => navigate('/product?=' + product.id, e);
 
@@ -79,32 +93,46 @@ function cartProductCard(product: IProduct): HTMLElement {
   const priceContainer = document.createElement('div');
   priceContainer.className = 'card-pricing';
   const discountedPrice = document.createElement('p');
-  discountedPrice.className = 'card-pricing__discounted';
+  discountedPrice.classList.add('card-pricing__discounted', 'prices-container__discount');
   discountedPrice.textContent = `${(product.price * ((100 - product.discountPercentage) / 100)).toFixed(2)} USD`;
   priceContainer.append(discountedPrice);
   if (discountPercentage) {
     const originalPrice = document.createElement('p');
-    originalPrice.className = 'card-pricing__original';
+    originalPrice.classList.add('card-pricing__original', 'prices-container__original');
     originalPrice.textContent = product.price.toFixed(2) + ' USD';
     priceContainer.append(originalPrice);
   }
-  const plusMinus = createPlusMinusButtons();
-  buyOptions.append(priceContainer, plusMinus);
+  const plusMinus = createPlusMinusButtons(product, container);
+  buyOptions.append(removeButton, priceContainer, plusMinus);
 
   container.append(leftContainer, productInfo, buyOptions);
   return container;
 }
 
-function createPlusMinusButtons() {
+function createPlusMinusButtons(product: IProduct, productCardEl: HTMLDivElement) {
   const buttonsContainer = document.createElement('div');
-  buttonsContainer.classList.add('plus-minus__container');
+  buttonsContainer.classList.add('count-container');
   const minusButton = document.createElement('button');
-  minusButton.textContent = '-';
+  minusButton.classList.add('count-container__minus', 'count-container__button');
+  minusButton.textContent = '−';
   const countSpan = document.createElement('span');
-  countSpan.textContent = '1';
+  countSpan.textContent = String(store.cart.getCountById(product.id));
   const plusButton = document.createElement('button');
+  plusButton.classList.add('count-container__plus', 'count-container__button');
   plusButton.textContent = '+';
 
+  plusButton.addEventListener('click', () => {
+    store.cart.add(product.id, product.price);
+    countSpan.textContent = String(store.cart.getCountById(product.id));
+  });
+  minusButton.addEventListener('click', () => {
+    store.cart.deleteOne(product.id);
+    if (store.cart.getCountById(product.id)) {
+      countSpan.textContent = String(store.cart.getCountById(product.id));
+    } else {
+      productCardEl.remove();
+    }
+  });
   buttonsContainer.append(minusButton, countSpan, plusButton);
   return buttonsContainer;
 }
