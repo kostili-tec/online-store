2c96c3c750abc0b0634abb2bba65ade959df1b8b
ferka123 <ferka911@gmail.com>
2022-12-22 21:50:08 +0200

    fix: index.ts

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,16 +1,11 @@
 import '../styles/main.scss';
-// import { createHeader } from './components/header';
-// import { createFooter } from './components/footer';
-// import { root } from './router';
+import { createHeader } from './components/header';
+import { createFooter } from './components/footer';
+import { root } from './router';
 import './events';
-import { Checkout } from './components/Checkout';
+
 const start = () => {
-  document.body.append(
-    Checkout(() => {
-      console.log('first');
-    }),
-  );
-  //document.body.append(createHeader(), root, createFooter());
+  document.body.append(createHeader(), root, createFooter());
 };
 
 start();
