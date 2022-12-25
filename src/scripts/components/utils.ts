f3a9a26220bc5b6aafc4dbce7fc165b2e471cb59
kostili-tec <weh4teyou@gmail.com>
2022-12-25 21:29:41 +0300

    feat: add createSvg function

--- src/scripts/components/utils.ts
+++ src/scripts/components/utils.ts
@@ -1,7 +1,18 @@
+import sprite from '../../assets/svg/sprite.svg';
+
 export const createElement = (tagName: string, options?: Partial<HTMLElement>): HTMLElement => {
   const element = document.createElement(tagName);
   if (options) {
     Object.assign(element, options);
   }
   return element;
+};
+
+export const createSvg = (classSvg: string, idSvg: string) => {
+  const svgEl = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  svgEl.classList.add('icon-svg', `${classSvg}`);
+  const useEl = document.createElementNS('http://www.w3.org/2000/svg', 'use');
+  useEl.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', `${sprite}#${idSvg}`);
+  svgEl.appendChild(useEl);
+  return svgEl;
 };
